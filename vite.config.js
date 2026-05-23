1a62409f15f6b37500ced85efaef714c53de5fce
jackie <jackie@example.com>
2026-05-23 20:11:12 +0800

    fix: set base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: true,
+    sourcemap: false,
   },
   server: {
     port: 5173,
